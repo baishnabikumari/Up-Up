f6de7200c9df7b77d0e43c77ea1658a5ffcfa1de
baishnabikumari <baishnabi.5.06.2011@gmail.com>
2026-01-19 18:29:05 +0530

    script is now working and giving the output that the player is actually died but the animationof die is not added yet to the script so my next work is to call the animation of die in the funciton inside the player script

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,25 +2,23 @@ extends Area2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var timer: Timer = $Timer
 
+signal player_died
 const SPEED = 100.0
 var direction = 1.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animated_sprite_2d.play("running")
-	timer.wait_time = 2.0
-	timer.one_shot = false
-	if not timer.timeout.is_connected(_on_timer_timeout):
-		timer.timeout.connect(_on_timer_timeout)
-		timer.start()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	position.x += direction * SPEED * delta
 	
 func _on_timer_timeout() -> void:
 	direction *= -1
 	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
-	print(direction)
-	
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		emit_signal("player_died", body)
---
+++ scripts/main.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_setup_level()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _setup_level() -> void:
+	var enemies = $Levels.get_node_or_null("Enemies")
+	if enemies:
+		for enemy in enemies.get_children():
+			enemy.player_died.connect(_on_player_died)
+			
+#signal handle
+func _on_player_died(body):
+	print(body)
+	print("Player died")
